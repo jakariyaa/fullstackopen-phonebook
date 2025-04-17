f2b089146a209134fced2ce6e5a80aef85f2038a
jakariyaa <jakariya3460@gmail.com>
2025-04-17 14:15:26 +0600

    add 3.18

--- index.js
+++ index.js
@@ -12,15 +12,10 @@ morgan.token('req-body', (request) => {
   return request.body ? JSON.stringify(request.body) : ''
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+app.use(morgan(':method :url :status :res[content-length] - ' +
+  ':response-time ms :req-body'))
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(person => {
-    response.json(person)
-  })
-})
-
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   const now = new Date().toString()
   Person.countDocuments({}).then(result => {
     response.send(
@@ -28,29 +23,22 @@ app.get('/info', (request, response) => {
       `</br>` +
       `<div>${now}</div>`
     )
-  })
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  Person.findById(id).then(person => {
-    person ? response.json(person) : response.status(404).end()
   }).catch(error => {
-    response.status(400).json({
-      error: 'Invalid ID',
-      message: error.message
-    })
+    console.log(error);
+    next(error)
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  Person.findByIdAndDelete(id).then(deleted => {
-    response.status(204).json(deleted)
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(person => {
+    response.json(person)
+  }).catch(error => {
+    console.log(error);
+    next(error)
   })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if (!body || !body.name || !body.number) {
     return response.status(400).json({
@@ -72,10 +60,73 @@ app.post('/api/persons', (request, response) => {
 
       person.save().then(savedPerson => {
         response.status(201).json(savedPerson)
+      }).catch(error => {
+        console.log(error);
+        next(error)
       })
     }
+  }).catch(error => {
+    console.log(error);
+    next(error)
   })
 })
+
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findById(id).then(person => {
+    person ? response.json(person) : response.status(404).end()
+  }).catch(error => {
+    console.log(error);
+    next(error)
+  })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findById(id).then(person => {
+    if (!person) {
+      return response.status(404).end()
+    }
+    person.number = request.body.number
+    person.save().then(updatedPerson => {
+      response.json(updatedPerson)
+    }).catch(error => {
+      console.log(error);
+      next(error)
+    })
+  }).catch(error => {
+    console.log(error);
+    next(error)
+  })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findByIdAndDelete(id).then(result => {
+    response.status(204).end()
+  }).catch(error => {
+    console.log(error);
+    next(error)
+  })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).json({
+      error: 'Invalid ID Format'
+    })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
